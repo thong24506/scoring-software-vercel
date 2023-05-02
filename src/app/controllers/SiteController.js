01954c3e51d62caf5a573cc2c5d64415b5b035fa
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-05-02 22:57:16 +0700

    up load pug user

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -4,7 +4,7 @@ const { mutipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     //[GET] /
     show(req, res, next) {
-        User.findOne({login: true}).lean()
+        User.findOne({userId: req.params.userId, login: true}).lean()
             .then(user => {
                 Point.find({userId: req.params.userId}).lean()
                     .then(points => res.render('home', { points, user }))
